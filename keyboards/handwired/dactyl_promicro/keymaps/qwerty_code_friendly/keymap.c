395336b6ec4d348e4770ab6bba492ef780f1f01b
Campbell Barton <ideasman42@gmail.com>
2020-12-23 11:29:59 +1100

    TEST

--- keyboards/handwired/dactyl_promicro/keymaps/qwerty_code_friendly/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/qwerty_code_friendly/keymap.c
@@ -201,7 +201,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   V  |   B  |       |   N  |   M  |   ,  |   .  |  /   | Delt |
  * '------+------+------+------+------+------'       '------+------+------+------+------+------'
- *               |Super |ScrLck|                                   | PgUp | PgDn |
+ *               |Super | PgUp |                                   | PgDn | Ins  |
  *               '-------------'                                   '-------------'
  *
  *               .------+------. .-------------.   .-------------. .------+------.
@@ -241,7 +241,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,           KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,
  MO(LAYER_DIRS),           KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
         KC_LSFT,           KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,
-        KC_TRNS,        KC_TRNS,        KC_LGUI,        KC_SLCK,        KC_TRNS,
+        KC_TRNS,        KC_TRNS,        KC_LGUI,        KC_PGUP,        KC_TRNS,
                                                                         KC_LPRN,        KC_LCBR,
                                                                                         KC_LBRC,
                                                          KC_SPC, MO(LAYER_KPAD),       KC_LCTRL,
@@ -251,7 +251,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,        KC_BSLS,
            KC_H,           KC_J,           KC_K,           KC_L,        KC_SCLN,        KC_QUOT,
            KC_N,           KC_M,        KC_COMM,         KC_DOT,        KC_SLSH,        KC_DELT,
-                        KC_TRNS,        KC_PGUP,        KC_PGDN,        KC_TRNS,        KC_TRNS,
+                        KC_TRNS,        KC_PGDN,         KC_INS,        KC_TRNS,        KC_TRNS,
         KC_RCBR,        KC_RPRN,
         KC_RBRC,
         KC_LALT, MO(LAYER_MDIA),         KC_ENT
