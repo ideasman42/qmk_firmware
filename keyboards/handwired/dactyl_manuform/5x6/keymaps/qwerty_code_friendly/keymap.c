26c3d692d70b986d491afb2abe1f89d53d32ffc9
Campbell Barton <ideasman42@gmail.com>
2025-05-02 21:29:15 +1000

    swap backspace/del & page keys

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/qwerty_code_friendly/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/qwerty_code_friendly/keymap.c
@@ -210,7 +210,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   V  |   B  |       |   N  |   M  |   ,  |   .  |  /   |Shift |
  * '------+------+------+------+------+------'       '------+------+------+------+------+------'
- *               | PgDn | PgUp |                                   |BSpace| Delt |
+ *               |BSpace| Delt |                                   | PgDn | PgUp |
  *               '-------------'                                   '-------------'
  *
  *               .------+------. .-------------.   .-------------. .------+------.
@@ -224,7 +224,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,           KC_Q,           KC_W,           KC_E,           KC_R,           KC_T, /*  */           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,        KC_BSLS,
          KC_ESC,           KC_A,           KC_S,           KC_D,           KC_F,           KC_G, /*  */           KC_H,           KC_J,           KC_K,           KC_L,        KC_SCLN,        KC_QUOT,
         KC_LSFT,           KC_Z,           KC_X,           KC_C,           KC_V,           KC_B, /*  */           KC_N,           KC_M,        KC_COMM,         KC_DOT,        KC_SLSH,        KC_RSFT,
-                                        KC_PGDN,        KC_PGUP,                                 /*  */                                        KC_BSPC,         KC_DEL,
+                                        KC_BSPC,         KC_DEL,                                 /*  */                                        KC_PGDN,        KC_PGUP,
                                                                          KC_SPC,        KC_LPRN, /*  */        KC_RPRN,         KC_ENT,
                                                                         KC_LBRC,        KC_LALT, /*  */ MO(LAYER_FKEY),        KC_RBRC,
                                                                  MO(LAYER_KPAD),        KC_LCTL, /*  */        KC_LGUI,  MO(LAYER_DIRS)
